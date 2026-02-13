6a6e817ecef92643681308ab4f4867639320841f
jgnooo <ehwk920@gmail.com>
2026-02-13 15:57:35 +0900

    Feat: Update VkProvider interface methods.

--- src/vulkan/vkprovider.h
+++ src/vulkan/vkprovider.h
@@ -7,7 +7,14 @@
 class VkProvider
 {
 public:
-    virtual VkContext& getContext() = 0;
+    virtual VkContext&      getContext()                                               = 0;
+    virtual VkCommandPool   getCommandPool(QueueType type)                             = 0;
+    virtual VkQueue         getQueue(QueueType type)                                   = 0;
+    virtual VkSwapchain&    getSwapchain()                                             = 0;
+    virtual VkExtent2D      getSwapchainExtent()                                       = 0;
+    virtual VkFormat        getSwapchainFormat()                                       = 0;
+    virtual VkCommandBuffer beginSingleTimeCommands(QueueType type)                    = 0;
+    virtual void            endSingleTimeCommands(VkCommandBuffer cmd, QueueType type) = 0;
     
     virtual ~VkProvider() = default;
 };
